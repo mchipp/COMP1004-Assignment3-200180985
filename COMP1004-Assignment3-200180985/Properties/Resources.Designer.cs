69da4f65389553450628a8deb79feb6893a8ec5c
Mark <mark.chipp@mygeorgian.ca>
2016-11-11 14:01:58 -0500

    SplashScreen image added

--- COMP1004-Assignment3-200180985/Properties/Resources.Designer.cs
+++ COMP1004-Assignment3-200180985/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace COMP1004_Assignment3_200180985.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap mbtitle {
+            get {
+                object obj = ResourceManager.GetObject("mbtitle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap serenity {
             get {
                 object obj = ResourceManager.GetObject("serenity", resourceCulture);
--- COMP1004-Assignment3-200180985/SplashScreenForm.Designer.cs
+++ COMP1004-Assignment3-200180985/SplashScreenForm.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.SplashScreenTimer = new System.Windows.Forms.Timer(this.components);
+            this.SplashScreenPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SplashScreenPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // SplashScreenTimer
@@ -38,15 +40,26 @@
             this.SplashScreenTimer.Interval = 3000;
             this.SplashScreenTimer.Tick += new System.EventHandler(this.SplashScreenTimer_Tick);
             // 
+            // SplashScreenPictureBox
+            // 
+            this.SplashScreenPictureBox.BackgroundImage = global::COMP1004_Assignment3_200180985.Properties.Resources.mbtitle;
+            this.SplashScreenPictureBox.Location = new System.Drawing.Point(25, 25);
+            this.SplashScreenPictureBox.Name = "SplashScreenPictureBox";
+            this.SplashScreenPictureBox.Size = new System.Drawing.Size(550, 250);
+            this.SplashScreenPictureBox.TabIndex = 0;
+            this.SplashScreenPictureBox.TabStop = false;
+            // 
             // SplashScreenForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 300);
+            this.Controls.Add(this.SplashScreenPictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SplashScreenForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreenForm";
+            ((System.ComponentModel.ISupportInitialize)(this.SplashScreenPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Timer SplashScreenTimer;
+        private System.Windows.Forms.PictureBox SplashScreenPictureBox;
     }
 }
